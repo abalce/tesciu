f946ea2607043497e3a9d3f702eb02cedbeceeea
kasia <ul0275820@edu.uni.lodz.pl>
2024-04-15 19:40:35 +0200

    dodanie folderu szkola/kierunek na firebase

--- app/src/main/java/com/example/studysorter/navigation/NavGraph.kt
+++ app/src/main/java/com/example/studysorter/navigation/NavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.studysorter.screens.HomeScreen
+
 import com.example.studysorter.screens.ProfileScreen
 import com.example.studysorter.screens.PrzedmiotyScreen
 import com.example.studysorter.screens.UlubioneScreen
@@ -25,7 +26,7 @@ fun SetUpNavGraph(
             ProfileScreen()
         }
         composable(Screens.Przedmioty.route){
-            PrzedmiotyScreen(innerPadding = innerPadding)
+            PrzedmiotyScreen()
         }
         composable(Screens.Ulubione.route){
             UlubioneScreen(innerPadding = innerPadding)
